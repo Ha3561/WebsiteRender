c76a205cc9788b9374580ad61a7c73a0cd889a01
Ha3561 <onmailharshit@gmail.com>
2024-04-05 00:20:02 +0530

    sixth commit

---
+++ run.py
@@ -0,0 +1,4 @@
+from ToDo import app  # Replace 'your_flask_app' with the actual name of your Flask application object
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # You can modify the host and port as needed
