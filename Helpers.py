aa6a56815fbd9ea34f86e29b10dc6b0d6465f9cf
Ha3561 <onmailharshit@gmail.com>
2024-04-05 01:19:27 +0530

    8th commit

--- Helpers.py
+++ Helpers.py
@@ -15,10 +15,10 @@ today = dt.now()
 today_day_month = today.strftime('%d-%m') 
 
 #openpyxl code
-path=r"C:\Users\Tanmay\Desktop\Personal\Network\Nework.xlsx"  #raw string to prevent escaping of characters
-wb=openpyxl.load_workbook(path)
-sheet=wb.active
-max_row=sheet.max_row
+#path=r"C:\Users\Tanmay\Desktop\Personal\Network\Nework.xlsx"  #raw string to prevent escaping of characters
+#wb=openpyxl.load_workbook(path)
+#sheet=wb.active
+#max_row=sheet.max_row
 
 import time
 
