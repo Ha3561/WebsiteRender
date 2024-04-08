d09fb48b7a3bd4ff434a346c60ccface51c13b3c
Ha3561 <onmailharshit@gmail.com>
2024-04-08 21:11:43 +0530

    Hadn't saved changes to last commit for web api key

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from ToDo import app  # Replace 'your_flask_app' with the actual name of your Flask application object
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=5000)  # You can modify the host and port as needed 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)  # You can modify the host and port as needed 
+    #app.run(debug=True)
